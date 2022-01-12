5c212c99ca35cc7a6f86b635032a28535677ce9c
Kyle <kylei.tw@gmail.com>
2022-01-12 22:51:10 +0800

    Initial Commit

---
+++ iOSClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSClock
+//
+//  Created by Kyle Lei on 2022/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
